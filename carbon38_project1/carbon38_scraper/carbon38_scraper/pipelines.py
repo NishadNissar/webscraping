17edcfccf5d2d69db66e43d977892877f0d5aed2
NishadNissar <nsd6145@gmail.com>
2025-06-25 08:42:56 +0530

    Refactor product pipeline to clean brand names by stripping whitespace and capitalizing

--- carbon38_project1/carbon38_scraper/carbon38_scraper/pipelines.py
+++ carbon38_project1/carbon38_scraper/carbon38_scraper/pipelines.py
@@ -1,13 +1,7 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
-
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-
-
-class Carbon38ScraperPipeline:
+import re
+class ProductCleanerPipeline:
     def process_item(self, item, spider):
-        return item
+        # Clean brand
+        if item.get('brand'):
+            item['brand'] = item['brand'].strip().title()
+
